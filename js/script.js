1051f77f70d653112be804c5d31e592a674ebbdb
ceccobell <francescobell2001@gmail.com>
2024-07-09 13:09:38 +0200

    [UPD] added loop to insert random numbers into array with check

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 function generateRandomNums() {
     return Math.floor(Math.random() * 100 + 1)
 }
+
+let random_nums = []
+while (random_nums.length < 5) {
+    let generate_num = generateRandomNums()
+    if (!random_nums.includes(generate_num)) {
+        random_nums.push(generate_num)
+    }
+}
+console.log(random_nums)
